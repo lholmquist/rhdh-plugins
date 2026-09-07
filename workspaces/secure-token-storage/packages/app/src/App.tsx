f3bf294f7fd78aaa18ab63b6a4bd565b224d53e0
Lucas Holmquist <lholmqui@redhat.com>
2026-09-07 14:18:32 -0400

    squash: add orchestrtor to the sample app

--- workspaces/secure-token-storage/packages/app/src/App.tsx
+++ workspaces/secure-token-storage/packages/app/src/App.tsx
@@ -6,8 +6,19 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import userSettingsPlugin from '@backstage/plugin-user-settings/alpha';
+import orchestratorPlugin, {
+  orchestratorTranslationsModule,
+} from '@red-hat-developer-hub/backstage-plugin-orchestrator';
+import orchestratorFormWidgetsPlugin from '@red-hat-developer-hub/backstage-plugin-orchestrator-form-widgets';
 import { rhdhThemeModule } from '@red-hat-developer-hub/backstage-plugin-theme/alpha';
 
 export default createApp({
-  features: [rhdhThemeModule, catalogPlugin, userSettingsPlugin],
+  features: [
+    rhdhThemeModule,
+    catalogPlugin,
+    userSettingsPlugin,
+    orchestratorPlugin,
+    orchestratorTranslationsModule,
+    orchestratorFormWidgetsPlugin,
+  ],
 });
--- workspaces/secure-token-storage/packages/backend/src/index.ts
+++ workspaces/secure-token-storage/packages/backend/src/index.ts
@@ -11,6 +11,17 @@ backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-notifications-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend'));
+backend.add(import('@backstage/plugin-signals-backend'));
+backend.add(
+  import('@red-hat-developer-hub/backstage-plugin-orchestrator-backend'),
+);
+backend.add(
+  import(
+    '@red-hat-developer-hub/backstage-plugin-scaffolder-backend-module-orchestrator'
+  ),
+);
 backend.add(
   import(
     '@red-hat-developer-hub/backstage-plugin-secure-token-storage-backend'
